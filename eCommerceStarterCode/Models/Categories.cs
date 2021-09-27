2ea624fe840c96a345da43605303a8c726d0c585
Treveon Edwards <fasuke322@gmail.com>
2021-09-27 17:04:37 -0400

    added categories class

---
+++ eCommerceStarterCode/Models/Categories.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Models
+{
+    public class Categories
+    {
+        public int CategoryID { get; set; }
+        public string  Tires { get; set; }
+        public string Mufflers { get; set; }
+        public string Rim { get; set; }
+        
+        
+
+    }
+}
