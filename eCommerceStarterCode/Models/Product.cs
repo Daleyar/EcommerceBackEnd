e01689961f4b7ee44f7d8ddab7937045edbe20b9
Ali Daley <ali.daley72@gmail.com>
2021-09-27 16:52:42 -0400

    Created the ShoppingCart model to follow the ERD using Foreign Key relationships

--- eCommerceStarterCode/Models/Product.cs
+++ eCommerceStarterCode/Models/Product.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace eCommerceStarterCode.Models
 {
-    public class Products
+    public class Product
     {
         public int ProductID {get; set;}
         public string ProductName{ get; set; }
