24fc64130c1295f82402a175fbb4561bc380492b
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-09-27 16:18:01 -0400

    Added Customer and Products to models

---
+++ eCommerceStarterCode/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BillingAddress { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddr { get; set; }
+    }
+}
