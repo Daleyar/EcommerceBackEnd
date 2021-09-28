ef977484f7d03f51cdf97274723939f6011b344c
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-09-28 17:01:40 -0400

    Added ProductController: untested and unfinished

--- eCommerceStarterCode/Controllers/ProductController.cs
+++ eCommerceStarterCode/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -25,8 +26,54 @@ namespace eCommerceStarterCode.Controllers
         {
             var products = _context.Products;
             return Ok(products);
-
         }
+
+        //GET BY ID
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var products = _context.Products.FirstOrDefault(product => product.ProductID == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+            return Ok(products);
+        }
+
+        //POST NEW PRODUCT
+        [HttpPost]
+        public IActionResult Post([FromBody] Product value)
+        {
+            _context.Products.Add(value);
+            _context.SaveChanges();
+            return Ok(value);
+        }
+
+        //PUT
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Product value)
+        {
+            var product = _context.Products.FirstOrDefault(product => product.ProductID == id);
+            product.Name = value.Name;
+            product.Price = value.Price;
+            product.Description = value.Description;
+            product.AverageRating = value.AverageRating;
+            product.CategoryID = value.CategoryID;
+            _context.SaveChanges();
+            return Ok(product);
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var product = _context.Products.FirstOrDefault(product => product.ProductID == id);
+            _context.Remove(product);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+
 
 
     }
